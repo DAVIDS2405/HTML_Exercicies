35315a103f48140db5901cf2196c0295caa5b9b2
David Basantes <sebastian2405lucero@hotmail.com>
2023-02-11 00:07:40 -0500

    Fronted mentor challeng

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.card-1 .vfd', {
+  strings: ["Verified Graduate.", "Verified Person."],
+  typeSpeed: 100,
+  showCursor: false
+});
+var typed2 = new Typed('.card-2 .card_user .vfd', {
+  strings: ['<p class="vfd">Verified Graduate</p>',' Verified Person.'],
+  typeSpeed: 100,
+  showCursor: false
+});
